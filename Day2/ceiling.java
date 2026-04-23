fb4ab7f006c473276959fcf00246d599971a7a4a
Raul Fernandes <raulferns1@gmail.com>
2026-04-23 16:54:50 +0530

    Day 3

--- Day2/ceiling.java
+++ Day2/ceiling.java
@@ -1,3 +1,4 @@
+package Day2;
 
 
 public class ceiling {
---
+++ FirstLastOccurence.java
@@ -0,0 +1,40 @@
+class FirstLastOccurence {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1,-1};
+        int start = searchFirstandLast(nums, target, true);
+        int end = searchFirstandLast(nums, target, false);
+
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+
+    }
+        
+    int searchFirstandLast(int[] nums, int target, boolean isFirst){
+        int start=0;
+        int end= nums.length-1;
+        int ans=-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(target==nums[mid]){
+                ans=mid;
+                if(isFirst){
+                    end= mid-1;
+                }
+                else{
+                    start = mid +1;
+                }
+            }
+            else if(target<=nums[mid]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+        
+    }
+}
