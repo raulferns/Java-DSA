637a5ca09e32162c816a8d79d497b7af2ea34685
Raul Fernandes <raulferns1@gmail.com>
2026-04-25 13:21:52 +0530

    Day 5

---
+++ Day5/SplitArrayLargestSum.java
@@ -0,0 +1,40 @@
+package Day5;
+
+public class SplitArrayLargestSum {
+    public int splitArray(int[] nums, int k){
+        int start=0;
+        int end=0;
+
+        for(int i=0;i<nums.length;i++){
+            start=Math.max(start, nums[i]);
+            end+=nums[i];
+        }
+
+        while(start<end){
+            int mid = start + (end-start)/2;
+
+            int sum=0;
+            int pieces=1;
+            for(int num : nums){
+                if(sum+num>mid){
+                    sum=num;
+                    pieces++;
+                }
+                else{
+                    start+=num;
+                }
+            }
+            if(pieces>k){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+        }
+        return end;
+    }
+    public static void main(String[] args){
+
+    }
+    
+}
