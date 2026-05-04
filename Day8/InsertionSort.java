558765877e8bf4c00739e2182bc30e47e7471d13
RaulTheMiner17 <raulferns1@gmail..com>
2026-05-04 21:16:55 +0530

    Day 8

---
+++ Day8/InsertionSort.java
@@ -0,0 +1,22 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6};
+        insertionSort(arr);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
